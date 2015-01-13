3e92dab33bea3473d73252d8f0e0e4a72a116c5d
Jovin Schneider <schneiderj2@wit.edu>
2015-01-13 14:44:04 -0500

    Lab 1 is Done

---
+++ lab1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#include <fstream>
+
+#include <cstring>
+
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	ifstream inf;
+	ofstream outf;
+	int input, sum = 0;
+	inf.open(argv[1]);
+	if(inf.fail()) {
+		cout << "Input file failed to open" << endl;
+		return 1;
+	}
+	outf.open("lab1output.txt");
+	if(outf.fail()) {
+		cout << "The output file failed to open" << endl;
+		return 1;
+	}
+	while(inf >> input) {
+		sum += input;
+		outf << input << "\t" << input * input << "\t" << sum << endl;
+	}
+	inf.close();
+	outf.close();
+	return 0;
+}
